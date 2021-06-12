a487cf3dac2e3d05112c9773237e2fa1a8725efd
ricassiocosta <ricassio_costa@outlook.com>
2021-06-12 11:37:01 -0300

    UI implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CartesianGrid, Scatter, ScatterChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Cluster } from './Cluster';
 import { kmeans } from './Kmeans';
+import { Button, ButtonsContainer, Container, SubTitle, Title } from './styles';
 import { getColor } from './utils/getColor';
 
 function App() {
+  const [selectedIteration, setSelectedIteration] = useState<Cluster[]>([])
+  const [iteration, setIteration] = useState(1)
+
+  useEffect(() => {
+    if(kmeans.history) {
+      setSelectedIteration(kmeans.history[0])
+    }
+  }, [])
+
+  function nextIteration() {
+    if(kmeans.history) {
+      let selectedIterationIndex = kmeans.history.indexOf(selectedIteration)
+      const nextIterationIndex = selectedIterationIndex + 1
+      const nextIteration = kmeans.history[nextIterationIndex]
+      setSelectedIteration(nextIteration)
+      setIteration(iteration + 1)
+    }
+  }
+
+  function previousIteration() {
+    if(kmeans.history) {
+      let selectedIterationIndex = kmeans.history.indexOf(selectedIteration)
+      const previousIterationIndex = selectedIterationIndex - 1
+      const nextIteration = kmeans.history[previousIterationIndex]
+      setSelectedIteration(nextIteration)
+      setIteration(iteration - 1)
+    }
+  }
+
   return (
-    <>
-      {
-        kmeans.history && kmeans.history.map((clusteredData) => (
-          <ScatterChart
-            width={400}
-            height={400}
-            margin={{
-              top: 20,
-              right: 20,
-              bottom: 20,
-              left: 20
-            }}
-            >
-              <CartesianGrid />
-              <XAxis type="number" dataKey="x" />
-              <YAxis type="number" dataKey="y"/>
-              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-              {
-                clusteredData.map((cluster, index) => (
-                  <Scatter name="Cluster" data={cluster.getDots()} fill={getColor(index)} shape="circle" />
-                ))
-              }
-              {
-                clusteredData.map((cluster, index) => (
-                  <Scatter name="Centroid" data={[cluster.getCentroid()]} fill={getColor(index)} shape="triangle"/>
-                ))
-              }
-            </ScatterChart>
-        ))
-      }
-    </>
+    <Container>
+      <Title>K-means</Title>
+      <SubTitle>{iteration}a iteração</SubTitle>
+      <ScatterChart
+        width={400}
+        height={400}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20
+        }}
+        >
+          <CartesianGrid />
+          <XAxis type="number" dataKey="x" />
+          <YAxis type="number" dataKey="y"/>
+          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+          {
+            selectedIteration.map((cluster, index) => (
+              <Scatter name="Cluster" data={cluster.getDots()} fill={getColor(index)} shape="circle" />
+            ))
+          }
+          {
+            selectedIteration.map((cluster, index) => (
+              <Scatter name="Centroid" data={[cluster.getCentroid()]} fill={getColor(index)} shape="triangle" />
+            ))
+          }
+        </ScatterChart>
+        <ButtonsContainer>
+          {kmeans.history && iteration > 1 && (
+            <Button onClick={previousIteration}>Previous Iteration</Button>
+          )}
+
+          {kmeans.history && iteration < kmeans.history.length && (
+            <Button onClick={nextIteration}>Next Iteration</Button>
+          )}
+        </ButtonsContainer>
+    </Container>
   );
 }
 
---
+++ src/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  width: 400px;
+  margin-left: 60px;
+  justify-content: space-evenly;
+`;
+
+export const Button = styled.button`
+  width: 120px;
+  height: 50px;
+`;
+
+export const Title = styled.h1`
+  font-family: 'Roboto', sans-serif;
+  margin-left: 60px;
+`;
+
+export const SubTitle = styled.h3`
+  font-family: 'Roboto', sans-serif;
+  margin-left: 60px;
+`;
