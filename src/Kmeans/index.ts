fc007a5fe013f74586249ab3564baa5b2b97ec5a
ricassiocosta <ricassio_costa@outlook.com>
2021-06-15 18:54:34 -0300

    setting professor dataset by default

--- src/Kmeans/index.ts
+++ src/Kmeans/index.ts
@@ -1,6 +1,6 @@
 import { Dot } from "../Dot";
 import { kmeans } from "./kmeans";
-import dataset from './dataset2.json';
+import dataset from './dataset.json';
 
 const dots: Dot[] = [];
 
@@ -8,6 +8,6 @@ for (const data of dataset) {
   dots.push(new Dot(data.x, data.y));
 }
 
-const clusteredData = kmeans(dots, 4);
+const clusteredData = kmeans(dots, 2);
 
 export { clusteredData as kmeans }
