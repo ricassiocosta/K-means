48a46b4e6e879b4297e19876dad6f41c252f70fc
ricassiocosta <ricassio_costa@outlook.com>
2021-06-15 19:20:22 -0300

    using clusters amount as constant

--- src/Kmeans/index.ts
+++ src/Kmeans/index.ts
@@ -3,11 +3,13 @@ import { kmeans } from "./kmeans";
 import dataset from './dataset.json';
 
 const dots: Dot[] = [];
+const clustersAmount = 2
 
 for (const data of dataset) {
-  dots.push(new Dot(data.x, data.y));
+  const dot = new Dot(data.x, data.y)
+  dots.push(dot);
 }
 
-const clusteredData = kmeans(dots, 2);
+const clusteredData = kmeans(dots, clustersAmount);
 
 export { clusteredData as kmeans }
